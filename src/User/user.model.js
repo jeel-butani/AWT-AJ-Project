86118e3583e8309b3361883ad2447fa51c05938c
jeel-butani <butanijeel1@gmail.com>
2024-04-14 14:25:45 +0530

    user model

---
+++ src/User/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  birthdate: {
+    type: Date,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  aadharCard: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  aadharCardImageUrl: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
