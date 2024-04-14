6d91388d109965eb82d1ec3d85527860b27e93cf
jeel-butani <butanijeel1@gmail.com>
2024-04-14 13:19:53 +0530

    connect mongo

--- index.js
+++ index.js
@@ -2,9 +2,34 @@ const express = require('express');
 const cors = require("cors");
 const app = express();
 const port = 3000;
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const dbConfig = require("./src/config/db.config");
+
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connection done with database");
+  })
+  .catch((err) => {
+    console.log("error in db connection ", err);
+    process.exit();
+  });
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
