11bce3d642504724d29b9eb1a587c5ec9990057b
jeel-butani <butanijeel1@gmail.com>
2024-04-17 21:32:56 +0530

    add apis method in repo

--- springBoot/driveease/src/main/java/com/example/driveease/repositories/CarRepository.java
+++ springBoot/driveease/src/main/java/com/example/driveease/repositories/CarRepository.java
@@ -13,4 +13,8 @@ import org.springframework.stereotype.Repository;
 public interface CarRepository extends MongoRepository<Car, ObjectId> {
     List<Car> findByCarName(String carName);
     Optional<Car> findById(String string);
+    List<Car> findByFuelType(String fuelType);
+    List<Car> findByTransmissionType(String transmissionType);
+    List<Car> findByCompanyName(String companyName);
+    
 }
