89c6be6cd80aacdd72dd1374dfacda479691645c
jeel-butani <butanijeel1@gmail.com>
2024-04-22 19:19:47 +0530

    service for generate token

--- src/Driver/driver.services.js
+++ src/Driver/driver.services.js
@@ -1,5 +1,5 @@
 const DriverModel = require("../Driver/driver.model");
-
+const crypto = require('crypto');
 exports.createDriver = async (driverData) => {
   const driver = new DriverModel(driverData);
   return await driver.save();
@@ -38,4 +38,22 @@ exports.getUserCount = async () => {
     console.error("Error getting user count:", err);
     throw err;
   }
+};
+
+exports.checkDriverExistsByEmailAndPassword = async (aadharCardNumber, password) => {
+  const driver = await DriverModel.findOne({ aadharCardNumber, password });
+  return driver;
+};
+
+exports.createSecretToken = async (id) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(32, (err, buffer) => {
+      if (err) {
+        reject(err);
+      } else {
+        const token = buffer.toString('hex');
+        resolve(token);
+      }
+    });
+  });
 };
