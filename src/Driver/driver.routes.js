4679a24c6ec4eb1f5574b0f2c4d044d92e3a487f
jeel-butani <butanijeel1@gmail.com>
2024-04-22 11:46:28 +0530

    api route for cout drivers

--- src/Driver/driver.routes.js
+++ src/Driver/driver.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const driverController = require("../Driver/driver.controller");
 
+router.get("/countDrivers", driverController.getDriverCount);
 router.post("/", driverController.createDriver);
 router.get("/", driverController.getAllDrivers);
 router.get("/name", driverController.getDriverByName);
