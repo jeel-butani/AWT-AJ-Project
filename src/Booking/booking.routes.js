0300966c7638d1c991db5e5d00fa0620d45d9a21
jeel-butani <butanijeel1@gmail.com>
2024-05-07 15:56:58 +0530

    add api path

--- src/Booking/booking.routes.js
+++ src/Booking/booking.routes.js
@@ -1,19 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const bookingController = require("../Booking/booking.controller");
-
-// router.post("/", bookingController.createBooking);
-// router.get("/", bookingController.getAllBookings);
-// router.get("/:id", bookingController.getBookingById);
-// router.put("/:id", bookingController.updateBooking);
-// router.delete("/:id", bookingController.deleteBooking);
-// router.get("/user", bookingController.getBookingByUserId);
-
-// module.exports = router;
-
-
-// booking.routes.js
-
 const express = require('express');
 const router = express.Router();
 const bookingController = require("./booking.controller");
@@ -24,5 +8,6 @@ router.get("/:id", bookingController.getBookingById);
 router.put("/:id", bookingController.updateBooking);
 router.delete("/:id", bookingController.deleteBooking);
 router.get("/user", bookingController.getBookingByUserId);
+router.get("/user/:userId", bookingController.getBookingsByUserId);
 
 module.exports = router;
